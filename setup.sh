ad96fdfbb871f1ed6e3aad3b7a189b88a9e7327d
rtx phantom <76226407+rtxphantom@users.noreply.github.com>
2023-08-09 09:20:03 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,19 +6,19 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
+# Install libssl and libnotify development packages
+echo "Installing libssl and libnotify development packages..."
+apt-get update
+apt-get install -y libssl-dev libnotify-dev
+if [ $? -ne 0 ]; then
+    echo "Failed to install required development packages."
+    exit 1
+fi
+
 # Compile the code
 gcc -o thefarmer thefarmer.c -lcrypto `pkg-config --cflags --libs libnotify`
 if [ $? -ne 0 ]; then
     echo "Compilation failed."
-    exit 1
-fi
-
-# Install prerequisites
-echo "Installing libnotify and libssl development packages..."
-apt-get update
-apt-get install -y libnotify-dev libssl-dev
-if [ $? -ne 0 ]; then
-    echo "Prerequisites installation failed."
     exit 1
 fi
 
